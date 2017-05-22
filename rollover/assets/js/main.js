4497fe11c5129449cac456b7b12178d81bca7398
jessicapang <jrsen2@student.monash.edu>
2017-05-23 09:16:29 +1000

    PUSH THIS SHIT

--- rollover/assets/js/main.js
+++ rollover/assets/js/main.js
@@ -4,54 +4,30 @@ $(document).ready(function() {
 // Add jQuery here
 
 $('.oase').mouseenter(function() {
- $('.oase__image').addClass('showme');
+  $('.oase__image').addClass('showme');
 });
 
+$('.oase').mouseleave(function() {
+  $('.oase__image').removeClass('showme');
+});
 
- $('.oase').mouseleave(function() {
-   $('.oase__image').removeClass('showme');
+ $('.c93').mouseenter(function() {
+  $('.c93__image').addClass('showme');
  });
 
 
+  $('.c93').mouseleave(function() {
+    $('.c93__image').removeClass('showme');
+  });
 
-
- $('.c93').mouseenter(function() {
-    $('.c93__image').addClass('showme');
+  $('.landscape').mouseenter(function() {
+   $('.landscape__image').addClass('showme');
   });
 
 
-    $('.c93').mouseleave(function() {
-      $('.c93__image').removeClass('showme');
-    });
-
-
-
-
-    $('.landscape').mouseenter(function() {
-       $('.landscape__image').addClass('showme');
-     });
-
-
-       $('.landscape').mouseleave(function() {
-         $('.landscape__image').removeClass('showme');
-       });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   $('.landscape').mouseleave(function() {
+     $('.landscape__image').removeClass('showme');
+   });
 
 
 
